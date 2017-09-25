ae596ce19d9c90899cf477af7ca4a4f3655e5079
jonathan dreyfuss <jdreyf@gmail.com>
2017-09-25 17:54:34 -0400

    saved ad_flux_mat to data

--- R/fluxPredict.R
+++ R/fluxPredict.R
@@ -7,10 +7,9 @@
 #'@param model An object of class \code{\link[sybil]{modelorg}}
 #'@param seahorse_data A data.frame returned by \code{\link{map_seahorse}}
 #'@param biomass_est Estimated biomass flux.  Not required, by may be useful.
-#'@param cores The number of cores to use for parallel computing.  If cores is greater than 1, a parallel backend
-#'must be registered.
+#'@details For parallel computing, a parallel backend must be registered. See \code{\link[foreach]{foreach}} for details.
 
-fluxPredict <-function(model, seahorse_data, biomass_est=0, cores=1){
+fluxPredict <-function(model, seahorse_data, biomass_est=0){
   final_output <- foreach(i=1:ncol(seahorse_data), .combine=cbind) %dopar% {
     exp_coefs <- c("EX_o2(e)in","EX_o2(e)ex","ATPS4m", "DM_atp_m_", "O2tm", "EX_lac_L(e)in", "EX_lac_L(e)ex", "biomass_reaction")  
     model_lb <- c(seahorse_data[grep("_lb$", rownames(seahorse_data)),i], biomass_est)
@@ -19,19 +18,18 @@ fluxPredict <-function(model, seahorse_data, biomass_est=0, cores=1){
     model <- changeObjFunc(model, "DM_atp_m_", obj_coef=1)    
     
     #If the constraints are not feasible, we return a column of NAs for those constraints
-    #for those constraints
     
     model_test_flux <- optimizeProb(model, algorithm="fba", lpdir="max")  
     lpsolution <- model_test_flux@lp_stat 
     
-    if(lpsolution == 0){
+    if (lpsolution == 0){
       output <- NA
-    } else{
+    } else {
       output_fluxes <- optimizeProb(model, alg="mtf", mtfobj=mod_obj(model_test_flux))
       output <- getFluxDist(output_fluxes)
     }
     output
-  }
+  }#end foreach
   rownames(final_output) <- react_id(model)
   final_output
 }
