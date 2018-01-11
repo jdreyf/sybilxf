b30ce0b4501d6350a0c7ac97cf61581432ea45d9
jonathan dreyfuss <jdreyf@gmail.com>
2018-01-10 20:14:41 -0500

    sd=std error

--- R/sample_seahorse.R
+++ R/sample_seahorse.R
@@ -1,13 +1,14 @@
 #'@title Sample from the seahorse data
-#'@author Alfred Ramirez
-#'@description This function takes the data.frame returned by the function \code{\link{summarize_seahorse}} and returns
-#'a data.frame with sampled measurements for the specified sample
+#'@author Alfred Ramirez, Jonathan Dreyfuss
+#'@description This function takes the data frame returned by the function \code{\link{summarize_seahorse}} and returns
+#'a data frame with sampled measurements for the specified sample
 #'@param x A data.frame
 #'@param sample.nm The sample name 
 #'@param nsamples The number of samples to generate
+#'@param nrep The number of replicates, used to calculate standard error
 #'@export
 
-sample_seahorse <- function(x, sample.nm, nsamples=150){
+sample_seahorse <- function(x, sample.nm, nsamples=150, nrep=3){
   if (!(sample.nm %in% rownames(x))){
     stop(paste0("The sample.nm '", sample.nm, "' must be in the rownames of x"))
   }
@@ -17,15 +18,15 @@ sample_seahorse <- function(x, sample.nm, nsamples=150){
   rownames(output_mat) <- c("OCR_basal", "OCR_oligo", "OCR_fccp", "OCR_rotenone", "PPR_basal", "PPR_oligo", "PPR_fccp", "PPR_rotenone")
   
   i <- sample.nm
-  output_mat["OCR_basal",] <- rnorm(nsamples,mean=x[i,"OCR_basal"],sd=x[i,"OCR_sd_basal"])
-  output_mat["OCR_oligo",] <- rnorm(nsamples,mean=x[i,"OCR_oligo"],sd=x[i,"OCR_sd_oligo"])
-  output_mat["OCR_fccp",] <- rnorm(nsamples,mean=x[i,"OCR_fccp"],sd=x[i,"OCR_sd_fccp"])
-  output_mat["OCR_rotenone",] <- rnorm(nsamples,mean=x[i,"OCR_rotenone"],sd=x[i,"OCR_sd_rotenone"])
+  output_mat["OCR_basal",] <- rnorm(nsamples, mean=x[i,"OCR_basal"], sd=x[i,"OCR_sd_basal"]/sqrt(nrep))
+  output_mat["OCR_oligo",] <- rnorm(nsamples, mean=x[i,"OCR_oligo"], sd=x[i,"OCR_sd_oligo"]/sqrt(nrep))
+  output_mat["OCR_fccp",] <- rnorm(nsamples, mean=x[i,"OCR_fccp"], sd=x[i,"OCR_sd_fccp"]/sqrt(nrep))
+  output_mat["OCR_rotenone",] <- rnorm(nsamples, mean=x[i,"OCR_rotenone"], sd=x[i,"OCR_sd_rotenone"]/sqrt(nrep))
   
-  output_mat["PPR_basal",] <- rnorm(nsamples,mean=x[i,"PPR_basal"],sd=x[i,"PPR_sd_basal"])
-  output_mat["PPR_oligo",] <- rnorm(nsamples,mean=x[i,"PPR_oligo"],sd=x[i,"PPR_sd_oligo"])
-  output_mat["PPR_fccp",] <- rnorm(nsamples,mean=x[i,"PPR_fccp"],sd=x[i,"PPR_sd_fccp"])
-  output_mat["PPR_rotenone",] <- rnorm(nsamples,mean=x[i,"PPR_rotenone"],sd=x[i,"PPR_sd_rotenone"])
+  output_mat["PPR_basal",] <- rnorm(nsamples, mean=x[i,"PPR_basal"], sd=x[i,"PPR_sd_basal"]/sqrt(nrep))
+  output_mat["PPR_oligo",] <- rnorm(nsamples, mean=x[i,"PPR_oligo"], sd=x[i,"PPR_sd_oligo"]/sqrt(nrep))
+  output_mat["PPR_fccp",] <- rnorm(nsamples, mean=x[i,"PPR_fccp"], sd=x[i,"PPR_sd_fccp"]/sqrt(nrep))
+  output_mat["PPR_rotenone",] <- rnorm(nsamples, mean=x[i,"PPR_rotenone"], sd=x[i,"PPR_sd_rotenone"]/sqrt(nrep))
   
   output_mat
 }
