9b636c6240df5652fcbfb90a2134b99e5d286f49
jonathan dreyfuss <jdreyf@gmail.com>
2018-01-10 20:14:20 -0500

    fluxPredict uses Rglpk or gurobi

--- R/fluxPredict.R
+++ R/fluxPredict.R
@@ -13,8 +13,9 @@
 #'@export
 
 fluxPredict <-function(model, seahorse_data, biomass_est=0, alg=c("fba", "mtf"), 
-                       obj_rxn=NULL, model.nm){
+                       obj_rxn=NULL, model.nm, solver=c("gurobi", "glpk")){
   alg <- match.arg(alg)
+  solver <- match.arg(solver)
   stopifnot(model.nm %in% c("2.1A", "2.1x", "2.2"))
   if (alg=="fba" && !is.null(obj_rxn) && !(obj_rxn %in% sybil::react_id(model))){
     stop("obj_rxn ", obj_rxn, " not in react_id(model).")
@@ -48,8 +49,13 @@ fluxPredict <-function(model, seahorse_data, biomass_est=0, alg=c("fba", "mtf"),
     #then found rxns in 2.1A by using grep w/ fixed=TRUE
     #L_LACt2r transports lac c <-> e
     if (model.nm=="2.2"){
-      lac.rxns <- c("L_LACt2r", "EX_lac_L(e)", "LDH_L", "LDH_Lm")
+      lac.rxns <- c("L_LACt2r", "EX_lac_L(e)", "LDH_L")
       co2.rxns <- c("CO2t", "CO2tm", "EX_hco3(e)", "H2CO3D", "H2CO3Dm", "r0941", "r1418")
+      model <- sybil::changeBounds(model, react=c("H2CO3D", "H2CO3Dm", "EX_lac_L(e)", "EX_hco3(e)"), lb=0)
+      model <- sybil::changeBounds(model, react=c("L_LACt2r", "LDH_L"), ub=0)
+      #no co2 ex
+      model <- sybil::changeBounds(model, react="EX_co2(e)", lb=0, ub=0)
+      
     } else if (model.nm %in% c("2.1A", "2.1x")){
       lac.rxns <- c("L_LACt2r", "EX_lac_L(e)ex", "LDH_L")
       co2.rxns <- c("CO2t", "CO2tm", "EX_hco3(e)ex", "H2CO3D", "r0941", "r1418")
@@ -78,9 +84,8 @@ fluxPredict <-function(model, seahorse_data, biomass_est=0, alg=c("fba", "mtf"),
       }
       output
     } else { 
-      #if not fba, then min weighted flux
+      #if not fba, then min weighted flux to nudge to hco3 + lactate efflux
       # output_fluxes <- sybil::optimizeProb(model, alg="mtf")
-      #min weighted flux to nudge to co2 + lactate efflux
       #output_fluxes <- sybil::optimizeProb(model, alg="fba", react=1:length(wts), obj_coef=wts)
       #output <- sybil::getFluxDist(output_fluxes)
       nrxns <- model@react_num
@@ -91,13 +96,26 @@ fluxPredict <-function(model, seahorse_data, biomass_est=0, alg=c("fba", "mtf"),
       mat2 <- abind_simple_sparse_array(eye, neg.eye, MARGIN = 2)
       mat3 <- abind_simple_sparse_array(neg.eye, neg.eye, MARGIN = 2)                           
       mat <- abind_simple_sparse_array(mat1, mat2, mat3, MARGIN=1)
+      mat <- as.simple_triplet_matrix(mat)
       
       obj <- c(numeric(nrxns), wts)
+      rhs <- numeric(nmets+2*nrxns)
       dirs <- rep(c("==", "<="), times=c(nmets, 2*nrxns))
-      lb <- list(ind = 1:nrxns, val = model@lowbnd)
-      ub <- list(ind =1:nrxns, val = model@uppbnd)
-      opt <- Rglpk_solve_LP(obj=obj, mat=mat, dir=dirs, rhs=numeric(nmets+2*nrxns), bounds=list(lower=lb, upper=ub), verbose=TRUE)
-      output <- opt$solution[1:nrxns]
+      if (solver=="glpk"){
+        lb <- list(ind = 1:nrxns, val = model@lowbnd)
+        ub <- list(ind = 1:nrxns, val = model@uppbnd)
+        opt <- Rglpk::Rglpk_solve_LP(obj=obj, mat=mat, dir=dirs, rhs=rhs, bounds=list(lower=lb, upper=ub))
+        if (opt$status==0) print("OPTIMAL SOLUTION FOUND")
+        output <- opt$solution[1:nrxns]
+      } else if (solver=="gurobi"){
+        sense <- sub("==", "=", dirs)
+        lb <- c(model@lowbnd, numeric(nrxns))
+        ub <- c(model@uppbnd, rep(1000, nrxns))
+        mod <- list(obj=obj, A=mat, rhs=rhs, sense=sense, lb=lb, ub=ub)
+        opt <- gurobi::gurobi(model=mod)
+        if (opt$status=="OPTIMAL") print("OPTIMAL SOLUTION FOUND")
+        output <- opt$x[1:nrxns]
+      }
     }
   }#end foreach
   rownames(final_output) <- sybil::react_id(model)
